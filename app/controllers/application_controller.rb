ad46a19944b197223296e4f8c2e1cb3b11e044ee
Niklas Karoly <niklas@karoly.io>
2021-09-04 17:11:25 +0200

    asddasdsa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def index
-    render json: { :test => true, :test2 => false }, :status => :ok
+    render json: { :test => true, :test2 => false, :test3 => true }, :status => :ok
   end
 end
